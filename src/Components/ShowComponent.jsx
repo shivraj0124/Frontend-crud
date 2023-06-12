6b16c2efddb363267615ec0091b82f254f28d346
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2023-06-13 01:26:17 +0530

    Update ShowComponent.jsx

--- src/Components/ShowComponent.jsx
+++ src/Components/ShowComponent.jsx
@@ -47,7 +47,7 @@ export default function ShowComponent() {
                                                 <td>{post.title}</td>
                                                 <td>{post.date}</td>
                                                 <td>
-                                                    <img src={'https://crud-backend-swart.vercel.app/api/postimages/'+post.image} style={{width:'100px',height:'100px'}} />
+                                                    <img src={'https://backend-crud-sk.vercel.app/api/postimages/'+post.image} style={{width:'100px',height:'100px'}} />
                                                 </td>
                                                 <td>
                                                     <button id={post._id} onClick={(e)=>handleDelete(post._id,e)} style={{backgroundColor:'blueviolet'}}>Delete</button>
